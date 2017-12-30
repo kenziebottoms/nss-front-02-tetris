d23131a31a0332fd6d61a08cbbeb5ae462152c67
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-29 23:02:11 -0600

    grid object accessible from main, piece object for grid's own use, grid can move down active piece, but canmovedown doesn't work right

---
+++ assets/js/Piece.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const blockMaps = {
+    "s": [
+        [1, 0],
+        [1, 1],
+        [0, 1]
+    ],
+    "t": [
+        [1, 0],
+        [1, 1],
+        [1, 0]
+    ],
+    "l": [
+        [1, 0],
+        [1, 0],
+        [1, 1],
+    ],
+    "j": [
+        [0, 1],
+        [0, 1],
+        [1, 1]
+    ],
+    "o": [
+        [1, 1],
+        [1, 1]
+    ],
+    "z": [
+        [0, 1],
+        [1, 1],
+        [1, 0]
+    ],
+    "i": [
+        [1],
+        [1],
+        [1],
+        [1]
+    ]
+};
+
+const colorCodes = {
+    "i": "red",
+    "l": "green",
+    "z": "purple",
+    "j": "yellow",
+    "o": "orchid",
+    "s": "blue",
+    "t": "orange"
+};
+
+function Piece(letter, origin) {
+    this.letter = letter;
+    this.map = blockMaps[letter];
+    this.color = colorCodes[letter];
+    this.origin = origin;
+}
+
+module.exports = {Piece};
