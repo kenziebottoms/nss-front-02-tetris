8dc15771278195680e19fe9c2319aa0ca71a5b40
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-30 20:23:52 -0600

    deleted code made obsolete by refactoring

--- assets/js/Grid.js
+++ assets/js/Grid.js
@@ -15,40 +15,11 @@ function Grid() {
     this.activePiece = {};
     this.addPiece = (letter, origin) => {
         let piece = new PieceFactory.Piece(letter, origin);
-        this.population.push(piece);
-        this.activePiece = piece;
-        // TODO: move to refresh grid fn probably
-        // loop through piece.map
-        for (let y=0; y<piece.map.length; y++) {
-            for (let x=0; x<piece.map[0].length; x++) {
-                // if this part of the letter is positive space
-                if (piece.map[y][x] == 1) {
-                    this.matrix[y+piece.origin[0]][x+piece.origin[1]] = piece.letter;
-                }
-            }
+        if (this.fits(piece, origin)) {
+            this.population.push(piece);
+            this.activePiece = piece;
+            this.place(piece, origin);
         }
-        draw.redraw(this.matrix);
-    };
-
-    this.canMoveDown = piece => {
-        let map = piece.map;
-        let origin = piece.origin;
-        // finds lowest positive space in map
-        for (let x=0; x<map[0].length; x++) {
-            for (let y=map.length-1; y>= 0; y--) {
-                if (map[y][x] == 1) {
-                    // checks if lowest positive space in map collides with other positive space
-                    if (this.matrix.length <= (origin[0]+y+1)) {
-                        return false;
-                    }
-                    if (this.matrix[origin[0]+y+1][origin[1]+x] != "") {
-                        return false;
-                    }
-                    break;
-                }
-            }
-        }
-        return true;
     };
     
     // moves active piece down one row
